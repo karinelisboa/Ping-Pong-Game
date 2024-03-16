f4646f2447c95f4c8d89cfb59070bc1675540dc6
karinelisboa <karinelisboarodrigues@gmail.com>
2024-03-16 17:50:15 -0300

    First player and screen added

--- main.py
+++ main.py
@@ -1,1 +1,38 @@
-Added file
+from turtle import Turtle, Screen
+
+screen = Screen()
+turtle = Turtle()
+
+screen.bgcolor("black")
+screen.setup(width=800, height=600)
+screen.title("Ping Pong Game")
+turtle.shapesize(stretch_wid=5, stretch_len=1)
+screen.tracer(0)
+turtle.shape("square")
+turtle.color("white")
+turtle.penup()
+turtle.goto(350, 0)
+
+
+screen.listen()
+
+def go_up():
+    new_y = turtle.ycor() + 30
+    turtle.goto(turtle.xcor(), new_y)
+
+def go_down():
+    new_y = turtle.ycor() - 30
+    turtle.goto(turtle.xcor(), new_y)
+
+screen.onkey(go_up, "Up")
+screen.onkey(go_down, "Down")
+
+game_is_on = True
+while game_is_on:
+    screen.update()
+
+
+
+
+
+screen.exitonclick()
