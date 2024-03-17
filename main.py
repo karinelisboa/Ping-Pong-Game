c3b0104e5bc060fb3e1fef3d01600da8c19e829c
karinelisboa <karinelisboarodrigues@gmail.com>
2024-03-17 08:42:59 -0300

    Added ball

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
 from turtle import Turtle, Screen
 from paddle import Paddle
+from ball import Ball
+import time
 
 screen = Screen()
 turtle = Turtle()
 
 screen.bgcolor("black")
-screen.setup(width=800, height=600)
+screen.setup(width=800, height=800)
 screen.title("Ping Pong Game")
 screen.tracer(0)
 
+ball = Ball()
 r_paddle = Paddle(350, 0)
 l_paddle = Paddle(-350, 0)
 
@@ -21,7 +24,9 @@ screen.onkey(l_paddle.go_down, "s")
 
 game_is_on = True
 while game_is_on:
+    time.sleep(0.1)
     screen.update()
+    ball.move()
 
 
 
